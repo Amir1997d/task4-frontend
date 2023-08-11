e13ee84f7190d84d8842980e6c9747813f85dfaf
Amir Dastouri <97881149+Amir1997d@users.noreply.github.com>
2023-08-11 14:33:37 +0300

    Update checkStatus.js

--- src/handlers/checkStatus.js
+++ src/handlers/checkStatus.js
@@ -1,5 +1,5 @@
 export default function checkStatus(username) {
-  return fetch(`http://localhost:5000/api/users/${username}`)
+  return fetch(`https://backend-adminpanel.onrender.com/api/users/${username}`)
     .then(res => res.json())
     .then(data => data.user_status)
     .catch(() => {
